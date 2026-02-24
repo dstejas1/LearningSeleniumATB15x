736d4bbf9e1d410baa576d32e3658402dbbe909c
Tejas DS <dstejas1@gmail.com>
2026-02-24 10:21:00 +0530

    New Selenium Baiscs

---
+++ src/test/java/TestSelenium05_WebDriver.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestSelenium05_WebDriver {
+    public static void main(String[] args) {
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://sdet.live");
+    }
+}
